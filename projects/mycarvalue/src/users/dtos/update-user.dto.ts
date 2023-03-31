663b8809fe9d5159a3ca952b1f305e7b51a68b5e
yeggadisaivivek <yeggadisaivivek@gmail.com>
2023-03-31 08:31:32 +0000

    added changes to users module

---
+++ projects/mycarvalue/src/users/dtos/update-user.dto.ts
@@ -0,0 +1,12 @@
+import { Optional } from '@nestjs/common';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsEmail()
+  @IsOptional()
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  password: string;
+}
